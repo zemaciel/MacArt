fbb01c576dc9b271181c423dce7a0bacaec91ed7
zemaciel <joemacbr@gmail.com>
2023-09-13 05:20:53 +0000

    Add header to base template

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,8 @@
-""" Home URL """
 from django.contrib import admin
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home/'),
+    path('', views.index, name='home')
+
 ]
