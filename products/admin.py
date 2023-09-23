d42732080102508e6ba9a24616b7603ac4eee21c
zemaciel <joemacbr@gmail.com>
2023-09-23 17:02:58 +0000

    Create new product model: Format

--- products/admin.py
+++ products/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Product, Category
+from .models import Product, Category, Format
 
 # Register your models here.
+
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = (
@@ -15,11 +16,21 @@ class ProductAdmin(admin.ModelAdmin):
 
     ordering = ('sku',)
 
+
 class CategoryAdmin(admin.ModelAdmin):
     list_display = (
         'friendly_name',
         'name',
     )
 
+
+class FormatAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Category, CategoryAdmin)
+admin.site.register(Format, FormatAdmin)
--- products/models.py
+++ products/models.py
@@ -5,9 +5,20 @@ class Category(models.Model):
 
     class Meta:
         verbose_name_plural = 'Categories'
-        
+
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Format(models.Model):
+    name = models.CharField(max_length=12)
+    friendly_name = models.CharField(max_length=12, null=True, blank=True)
 
     def __str__(self):
         return self.name
@@ -25,6 +36,7 @@ class Product(models.Model):
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
+    format = models.ForeignKey('Format', null=True, blank=True, on_delete=models.SET_NULL)
 
     def __str__(self):
         return self.name
