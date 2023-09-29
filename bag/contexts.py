b61087aaf8dd20534988378b47b945a63200aaeb
zemaciel <joemacbr@gmail.com>
2023-09-29 18:17:03 +0000

    Modity logic in bag contex.py and views.py

--- bag/contexts.py
+++ bag/contexts.py
@@ -17,12 +17,12 @@ def bag_contents(request):
             product_count += item_data
             bag_items.append({
                 'item_id': item_id,
-                'quantity': item_data,
+                'quantity': quantity,
                 'product': product,
             })
         else:
             product = get_object_or_404(Product, pk=item_id)
-            for size, quantity in item_data['items_by_size'].items():
+            for size, finish, quantity in item_data['items_by_size','items_by_finish'].items():
                 total += quantity * product.price
                 product_count += quantity
                 bag_items.append({
@@ -31,33 +31,25 @@ def bag_contents(request):
                     'product': product,
                     'size': size,
                 })
-            for finish, quantity in item_data.get('items_by_finish', {}).items():
-                total += quantity * product.price
-                product_count += quantity
-                bag_items.append({
-                    'item_id': item_id,
-                    'quantity': item_data,
-                    'product': product,
-                    'finish': finish,
-                })
 
-    if total < settings.FREE_DELIVERY_THRESHOLD:
-        delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
-        free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
-    else:
-        delivery = 0
-        free_delivery_delta = 0
-    
-    grand_total = delivery + total
-    
-    context = {
-        'bag_items': bag_items,
-        'total': total,
-        'product_count': product_count,
-        'delivery': delivery,
-        'free_delivery_delta': free_delivery_delta,
-        'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
-        'grand_total': grand_total,
-    }
 
-    return context
+        if total < settings.FREE_DELIVERY_THRESHOLD:
+            delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
+            free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
+        else:
+            delivery = 0
+            free_delivery_delta = 0
+        
+        grand_total = delivery + total
+        
+        context = {
+            'bag_items': bag_items,
+            'total': total,
+            'product_count': product_count,
+            'delivery': delivery,
+            'free_delivery_delta': free_delivery_delta,
+            'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
+            'grand_total': grand_total,
+        }
+
+        return context
