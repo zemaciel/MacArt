35232bf00bed6791406a42ab541a0bd23a79174a
zemaciel <joemacbr@gmail.com>
2023-10-01 11:54:13 +0000

    Start to add Stripe

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51NwNIpJrg7P3ry9omI78uxetjgZM3rvXoosSvi4e2qNwqSe2UcEGmcuB6JDi6HT1zAF3ZQqfh7BIfCoUDTpUIa8L00OMjwEYtW',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
