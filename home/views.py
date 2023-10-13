0e606218c814b1942b87413f6643b50b06d76700
zemaciel <joemacbr@gmail.com>
2023-10-13 12:29:13 +0000

    Set up of contact page

--- home/views.py
+++ home/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .forms import ContactForm
 
 def index(request):
     """ A view to return the index page """
     return render(request, 'home/index.html')
+
+
+def contact(request):
+    """View to the contact page (from: youtu.be/dnhEnF7_RyM) """
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            print('the form was valid')
+
+            return redirect('home')
+    else:
+        form = ContactForm()
+
+    return render(request, 'home/contact.html', {'form': form})
+
