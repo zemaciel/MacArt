d4643663557eff87bc1d052cf3158dc2425db7ae
zemaciel <joemacbr@gmail.com>
2023-10-14 10:54:27 +0000

    Set views, urls and initial templates for artists

---
+++ artists/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.all_artists, name='artists'),
+    path('<int:artist_id>/', views.artist_detail, name='artist_detail'),
+]
--- artists/views.py
+++ artists/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Artist
 
 # Create your views here.
+
+
+def all_artists(request):
+    """ A view to show all artists """
+
+    artists = Artist.objects.all()
+
+    context = {
+        'artists': artists,
+    }
+
+    return render(request, 'artists/artists.html', context)
+
+
+def artist_detail(request, artist_id):
+    """ A view to show individual artist details """
+
+    artist = get_object_or_404(Artist, pk=artist_id)
+
+    context = {
+        'artist': artist,
+    }
+
+    return render(request, 'artists/artist_detail.html', context)
