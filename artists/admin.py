b40471d09040938e5e230333e6ed9606025cc5fc
zemaciel <joemacbr@gmail.com>
2023-10-14 10:35:19 +0000

    Updated product models and artist admin

--- artists/admin.py
+++ artists/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Artist
 
-# Register your models here.
+
+class ArtistAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'bio',
+        'display_image',)
+
+
+admin.site.register(Artist)
