0066e1914e7e4216b398693d86dcf97f169ccd7c
zemaciel <joemacbr@gmail.com>
2023-10-21 17:29:15 +0000

    Add feature to shop owner to add artist

--- artists/views.py
+++ artists/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect, reverse
 from .models import Artist, SocialMedia
+from django.contrib import messages
 from .forms import ArtistForm, SocialMediaForm
 
 
@@ -31,8 +32,25 @@ def artist_detail(request, artist_id):
 
 def add_artist(request):
     """ Add an artist """
-    artist_form = ArtistForm()
-    social_media_form = SocialMediaForm()
+    
+    if request.method == 'POST':
+        artist_form = ArtistForm(request.POST, request.FILES)
+        social_media_form = SocialMediaForm(request.POST)
+        
+        if artist_form.is_valid() and social_media_form.is_valid():
+            artist = artist_form.save()
+
+            social_media = social_media_form.save(commit=False)
+            social_media.artist = artist
+            social_media.save()
+
+            messages.success(request, 'Successfully added new artist!')
+            return redirect(reverse('artist_detail', args=[artist.id]))
+        else:
+            messages.error(request, 'Failed to add new artist.')
+    else:
+        artist_form = ArtistForm()
+        social_media_form = SocialMediaForm()
 
     template = 'artists/add_artist.html'
     context = {
