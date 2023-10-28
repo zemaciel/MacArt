ad6794591701edb58c46d11eec755980f7b202a7
zemaciel <joemacbr@gmail.com>
2023-10-28 11:02:53 +0000

    Add funcionality to edit and delete FAQ Entries

--- faq/views.py
+++ faq/views.py
@@ -41,3 +41,42 @@ def add_faq(request):
 
     return render(request, template, context)
 
+
+@login_required
+def edit_faq(request, faq_id):
+    """ Edit a FAQ Entry """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only staff can do that.')
+        return redirect(reverse('home'))
+    faq = get_object_or_404(FAQEntry, pk=faq_id)
+    if request.method == 'POST':
+        form = FAQForm(request.POST, instance=faq)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully updated the entry!')
+            return redirect(reverse('FAQList'))
+        else:
+            messages.error(request, 'Error, check if the form is valid.')
+    else:
+        form = FAQForm(instance=faq)
+        messages.info(request, f'You are editing {faq.question}')
+
+    template = 'faq/edit_faq.html'
+    context = {
+        'form': form,
+        'faq': faq,
+    }
+
+    return render(request, template, context)
+
+
+@login_required
+def delete_faq(request, faq_id):
+    """ Delete a FAQ entry from the store """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only staff can do that.')
+        return redirect(reverse('home'))
+    faq = get_object_or_404(FAQEntry, pk=faq_id)
+    faq.delete()
+    messages.success(request, 'Entry deleted!')
+    return redirect(reverse('FAQList'))
