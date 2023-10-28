7d1d25605c8f109c8fecd429ba7b3d983fc873b8
zemaciel <joemacbr@gmail.com>
2023-10-28 17:05:05 +0000

    Remove secret key and set debug

--- mac_art/settings.py
+++ mac_art/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i0x+k8f(u=)$3j1=0^b!r_%=t%q%+wwv2!906g)4j5i!5d*1ow'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ 
 
 ALLOWED_HOSTS = ['pp5-macart-6c87c04aa952.herokuapp.com', '8000-zemaciel-macart-i45nj6ch919.ws-eu105.gitpod.io']
 
