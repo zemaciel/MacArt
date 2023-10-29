d267d29fbae4f299f3f38927e4da1aa2c6f6db7f
zemaciel <joemacbr@gmail.com>
2023-10-29 08:53:35 +0000

    Test deployment

--- mac_art/settings.py
+++ mac_art/settings.py
@@ -25,7 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ 
+# DEBUG = 'DEVELOPMENT' in os.environ 
+DEBUG = True 
 
 ALLOWED_HOSTS = ['pp5-macart-6c87c04aa952.herokuapp.com', '8000-zemaciel-macart-i45nj6ch919.ws-eu105.gitpod.io']
 
