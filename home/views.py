86e726c0136f163c979b979af13ed30d06afcfa3
zemaciel <joemacbr@gmail.com>
2023-11-04 15:13:44 +0000

    Issue in the main-nav bar

--- home/views.py
+++ home/views.py
@@ -31,7 +31,7 @@ def contact(request):
             })
 
             send_mail('The contact Form subject', 'This is the message',
-                      'noreply@macart.com', ['settings.DEFAULT_FROM_EMAIL'], html_message=html)
+                      'settings.DEFAULT_FROM_EMAIL', ['noreply@macart.com'], html_message=html)
 
             return redirect('emailsent')
     else:
